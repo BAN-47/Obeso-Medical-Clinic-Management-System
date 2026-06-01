0b4ecc9f67a426f41a7d1a2c4a35e5106f7f7133
Tristan Obeso <tristanobeso@gmail.com>
2026-06-01 19:54:32 +0800

    Plssssssss

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
+import os
 import mysql.connector
 
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'obeso_clinic_database'
-}
-
-
 def connect_db():
-    return mysql.connector.connect(**DB_CONFIG)
+    return mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT", 3306)),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE")
+    )
