5c82620edd577c2026a0c9e179c03808cc8752fd
Dignos, Christopher S. <christopherdignos90@gmail.com>
2026-06-02 18:45:36 +0800

    Refactor index.php for improved structure and clarity

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $conn = $database->connect();
     
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Obeso's Clinic Management System</title>
-<link rel="icon" type="image/png" href="/Obeso-Clinic-Management-System/Includes/favicon_obeso.png" />
+<link rel="icon" type="image/png" href="Obeso-Clinic-Management-System/Includes/favicon_obeso.png" />
 <script src="https://cdn.tailwindcss.com"></script>
 
 <style>
@@ -41,7 +41,7 @@ $conn = $database->connect();
     <!-- Logo / Title -->
     <div class="flex justify-center mb-8">
       <div class="flex items-center justify-center w-35 h-20 overflow-hidden bg-white shadow-md" style="width: 220px; height: 120px;">
-        <img src="/Obeso-Clinic-Management-System/Includes/Obeso_Med_Clinic_logo.png alt="Obeso's Clinic Logo" class="w-full h-full object-cover rounded-3xl">
+        <img src="Obeso-Clinic-Management-System/Includes/Obeso_Med_Clinic_logo.png" alt="Obeso's Clinic Logo" class="w-full h-full object-cover rounded-3xl">
       </div>
     </div>
 
@@ -51,7 +51,7 @@ $conn = $database->connect();
     </h2>
 
     <!-- Login Form -->
-    <form method="POST" action="/Obeso-Clinic-Management-System/Public/login_register.php" autocomplete="off" class="space-y-6">
+    <form method="POST" action="./Obeso-Clinic-Management-System/Public/login_register.php" autocomplete="off" class="space-y-6">
 
       <!-- Username -->
       <input
