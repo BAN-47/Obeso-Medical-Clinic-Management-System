9bb1021c2a67c035d10cf5ae603e5753575c62ec
Tristan Obeso <tristanobeso@gmail.com>
2026-06-02 10:37:04 +0800

    adsfdasf

--- ObesoClinicSys/config/db.php
+++ ObesoClinicSys/config/db.php
@@ -1,36 +1,31 @@
 <?php
 
 class Database {
-    private $host;
-    private $port;
-    private $dbname;
-    private $user;
-    private $pass;
     private $conn;
-
-    public function __construct() {
-        $this->host = getenv('DB_HOST');
-        $this->port = getenv('DB_PORT');
-        $this->dbname = getenv('DB_NAME');
-        $this->user = getenv('DB_USER');
-        $this->pass = getenv('DB_PASS') ?: "";
-    }
 
     public function connect() {
 
         if ($this->conn === null) {
 
+            $url = getenv("DATABASE_URL");
+
+            if (!$url) {
+                die("DATABASE_URL missing");
+            }
+
+            $db = parse_url($url);
+
+            $host = $db["host"];
+            $port = $db["port"] ?? 3306;
+            $user = $db["user"];
+            $pass = $db["pass"] ?? "";
+            $dbname = ltrim($db["path"], "/");
+
             try {
-                if (!$this->host || !$this->port || !$this->dbname || !$this->user) {
-                    die("Missing DB environment variables");
-                }
-
-                $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};charset=utf8";
-
                 $this->conn = new PDO(
-                    $dsn,
-                    $this->user,
-                    $this->pass
+                    "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+                    $user,
+                    $pass
                 );
 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
