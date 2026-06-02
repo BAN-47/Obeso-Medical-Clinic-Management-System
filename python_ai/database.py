dec47ed689d275dbe5831d375087eb1a192c03bf
BAN <christophedignos90@gmail.com>
2026-06-02 18:51:52 +0800

    another change

--- python_ai/database.py
+++ python_ai/database.py
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
+import os
 import mysql.connector
 
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'obeso_clinic_database'
+    'host': os.getenv('MYSQLHOST', 'localhost'),
+    'port': int(os.getenv('MYSQLPORT', 3306)),
+    'user': os.getenv('MYSQLUSER', 'root'),
+    'password': os.getenv('MYSQLPASSWORD', ''),
+    'database': os.getenv('MYSQLDATABASE', 'obeso_clinic_database')
 }
 
 
 def connect_db():
     return mysql.connector.connect(**DB_CONFIG)
-=======
-import os
-import mysql.connector
 
-def connect_db():
-    return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT", 3306)),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE")
-    )
->>>>>>> e86955d7fe0aa3f343472ba5e133bc8261b4ae4d
