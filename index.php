7530ed68e8a10b970beb494de46152c4bad1e9b0
Tristan Obeso <tristanobeso@gmail.com>
2026-06-02 11:18:33 +0800

    adssa

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $conn = $database->connect();
     <!-- Logo / Title -->
     <div class="flex justify-center mb-8">
       <div class="flex items-center justify-center w-35 h-20 overflow-hidden bg-white shadow-md" style="width: 220px; height: 120px;">
-        <img src="Obeso-Clinic-Management-System/Includes/Obeso_Med_Clinic_logo.png" alt="Obeso's Clinic Logo" class="w-full h-full object-cover rounded-3xl">
+        <img src="../Includes/Obeso_Med_Clinic_logo.png" alt="Obeso's Clinic Logo" class="w-full h-full object-cover rounded-3xl">
       </div>
     </div>
 
