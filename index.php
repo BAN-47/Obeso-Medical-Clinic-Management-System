40e03cf921c29f077195f57fb6515e2927658716
Tristan Obeso <tristanobeso@gmail.com>
2026-06-02 12:20:36 +0800

    7uyh45

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once __DIR__ . "./Config/database.php";
+require_once __DIR__ . "/Obeso-Clinic-Management-System/Config/database.php";
 
 $database = new Database();
 $conn = $database->connect();
