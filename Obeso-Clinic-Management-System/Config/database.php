f073a57f6c7ce791f42913f7af5f7a3ddb69c40c
Tristan Obeso <tristanobeso@gmail.com>
2026-06-03 19:28:48 +0800

    pulleddataba

--- Obeso-Clinic-Management-System/Config/database.php
+++ Obeso-Clinic-Management-System/Config/database.php
@@ -2,7 +2,7 @@
      class Database {
          private $host = "sql105.infinityfree.com";
           private $dbname = "if0_40936666_obeso_clinic_database";
-          private $username = "root";
+          private $username = "if0_40936666";
           private $password = "HlUf9MHo1WBXMk";
           private $conn;
 
