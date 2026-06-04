0be7188cc145912961177230858cea121114bf5c
Emmanuel D. Ugalde <macky.ugalde@gmail.com>
2026-06-04 09:27:47 +0800

    dunno18

--- Obeso-Clinic-Management-System/Config/database.php
+++ Obeso-Clinic-Management-System/Config/database.php
@@ -1,11 +1,10 @@
 <?php
      class Database {
-         private $host = "sql105.infinityfree.com";
-          private $dbname = "if0_40936666_obeso_clinic_database";
-          private $username = "if0_40936666";
-          private $password = "HlUf9MHo1WBXMk";
+          private $host = "localhost";
+          private $dbname = "obeso_clinic_database";
+          private $username = "root";
+          private $password = "";
           private $conn;
-
 
           public function connect() {
                if ($this->conn == null) {
